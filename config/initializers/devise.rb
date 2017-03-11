b4c2aaf4b48d820b6e4bb00ec2af48a40d2c208e
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-11 21:42:58 +0800

    Add config omni auth fb app id and app secret

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -249,7 +249,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
+  config.omniauth :facebook, "170403296803865", "4a6681d3ef1a0c7b24aa529034c8df69", :scope => 'public_profile,email', 
+  :info_fields => 'email,name', callback_url: "http://localhost:3000/users/auth/facebook/callback"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
