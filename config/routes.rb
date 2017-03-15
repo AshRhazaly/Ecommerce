6c05329b1dab9aa2b963ecbdd6fa4e273d1ede0d
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-16 07:48:29 +0800

    Create 3 controllers for payments,carts,line_items

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
+  get 'payments/new'
+
+  get 'payments/create'
+
+  get 'carts/new'
+
+  get 'carts/create'
+
+  get 'carts/edit'
+
+  get 'carts/update'
+
+  get 'carts/show'
+
+  get 'carts/destroy'
+
+  get 'line_items/create'
+
+  get 'line_items/destroy'
+
+  get 'line_items/update'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :products do
